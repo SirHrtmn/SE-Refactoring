4feb5ba08c353671b44ab52ef0efab9d916dc70b
Noah Hartmann <n.hartmann@seeburger.de>
2021-05-11 13:16:14 +0200

    Reordered Price class structure. (Inner classes instead outer classes)

--- src/main/pricing/Price.java
+++ src/main/pricing/Price.java
@@ -11,40 +11,47 @@ package main.pricing;
 public abstract class Price
 {
     public abstract double getPrice(int daysRented);
-    
+
     public class RegularPrice extends Price
     {
 
         @Override
         public double getPrice(int daysRented)
         {
-            return 0;
+            double result = 2.0;
+            if (daysRented > 2)
+            {
+                result += (daysRented - 2) * 1.5;
+            }
+            return result;
         }
-        
+
     }
-    
+
     public class NewReleasePrice extends Price
     {
 
         @Override
         public double getPrice(int daysRented)
         {
-            return 0;
+            return daysRented * 3.0;
         }
-        
+
     }
-    
+
     public class ChildrensPrice extends Price
     {
 
         @Override
         public double getPrice(int daysRented)
         {
-            return 0;
+            double result = 1.5;
+            if (daysRented > 3)
+            {
+                result += (daysRented - 3) * 1.5;
+            }
+            return result;
         }
-        
+
     }
 }
-
-
-
